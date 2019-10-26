e50c99bbf20c568f9e12e3ccb0c9588355bd2699
Alexey Salangin <a7ls@ya.ru>
2019-10-26 12:35:56 +0300

    Create Xcode project

---
+++ Surprise/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Surprise
+//
+//  Created by Alexey Salangin on 10/26/19.
+//  Copyright © 2019 Alexey Salangin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow()
+        self.window?.rootViewController = UIViewController()
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
